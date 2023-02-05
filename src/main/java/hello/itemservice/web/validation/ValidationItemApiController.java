f78172f00275b26bb9bf00e9df270be3f0cc601c
kimtaesoo99 <107785279+kimtaesoo99@users.noreply.github.com>
2023-02-05 15:26:55 +0900

    feat: api 컨트롤러 수정추가

--- src/main/java/hello/itemservice/web/validation/ValidationItemApiController.java
+++ src/main/java/hello/itemservice/web/validation/ValidationItemApiController.java
@@ -1,13 +1,11 @@
 package hello.itemservice.web.validation;
 
 import hello.itemservice.web.validation.form.ItemSaveForm;
+import hello.itemservice.web.validation.form.ItemUpdateForm;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/validation/api/items")
@@ -26,4 +24,20 @@ public class ValidationItemApiController {
         log.info("성공 로직 실행");
         return form;
     }
+
+
+    @PutMapping("/edit")
+    public Object editItem(@RequestBody @Validated ItemUpdateForm form, BindingResult bindingResult){
+        log.info("API 컨트롤러 호출");
+
+        if (bindingResult.hasErrors()){
+            log.info("겅증 오류 발생 errors ={}",bindingResult);
+            return bindingResult.getAllErrors();
+        }
+
+        log.info("성공 로직 실행");
+        return form;
+    }
+
+
 }
