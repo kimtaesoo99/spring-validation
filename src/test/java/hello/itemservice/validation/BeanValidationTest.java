1715526dd65d3c486490bc97eabcae1f45393c51
kimtaesoo99 <107785279+kimtaesoo99@users.noreply.github.com>
2023-02-05 13:51:46 +0900

    test: validation 테스트 추가

---
+++ src/test/java/hello/itemservice/validation/BeanValidationTest.java
@@ -0,0 +1,33 @@
+package hello.itemservice.validation;
+
+import hello.itemservice.domain.item.Item;
+import org.junit.jupiter.api.Test;
+
+import javax.validation.ConstraintViolation;
+import javax.validation.Validation;
+import javax.validation.Validator;
+import javax.validation.ValidatorFactory;
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class BeanValidationTest {
+
+    @Test
+    public void beanValidation() throws Exception{
+        ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
+        Validator validator = factory.getValidator();
+
+        Item item = new Item();
+        item.setItemName(" ");
+        item.setPrice(0);
+        item.setQuantity(10000);
+
+        Set<ConstraintViolation<Item>> violations = validator.validate(item);
+        for (ConstraintViolation<Item> violation : violations) {
+            System.out.println("violation = " + violation);
+            System.out.println("violation = " + violation.getMessage());
+        }
+        assertThat(violations).isNotEmpty();
+    }
+}
